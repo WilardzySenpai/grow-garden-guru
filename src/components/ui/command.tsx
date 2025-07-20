25c495ce923c8c7b02e2b924f5e974a46d5113b9
Hachiki <ninonakano408@gmail.com>
2025-07-20 13:57:02 +0000

    fix: Improve formatting and indentation in CommandDialog component

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -25,19 +25,19 @@ Command.displayName = CommandPrimitive.displayName
 interface CommandDialogProps extends DialogProps {}
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
-  return (
-    <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <VisuallyHidden>
-          <DialogTitle>Command Menu</DialogTitle>
-          <DialogDescription>Use the command menu to navigate and execute commands.</DialogDescription>
-        </VisuallyHidden>
-        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
-          {children}
-        </Command>
-      </DialogContent>
-    </Dialog>
-  )
+    return (
+        <Dialog {...props}>
+        <DialogContent className="overflow-hidden p-0 shadow-lg">
+            <VisuallyHidden>
+            <DialogTitle>Command Menu</DialogTitle>
+            <DialogDescription>Use the command menu to navigate and execute commands.</DialogDescription>
+            </VisuallyHidden>
+            <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+            {children}
+            </Command>
+        </DialogContent>
+        </Dialog>
+    )
 }
 
 const CommandInput = React.forwardRef<
