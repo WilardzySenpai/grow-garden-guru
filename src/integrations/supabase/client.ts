c0e70c19d0030a30fd9c79c92651eadebc17a3f2
chichi3819 <hoshinochichi1@gmail.com>
2025-07-21 15:24:25 +0800

    WIP: Supabase implemnetation

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://emufdclxlqzwhlcsvtjt.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtdWZkY2x4bHF6d2hsY3N2dGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NjExOTEsImV4cCI6MjA2NzAzNzE5MX0.ooFCI3njW3iSrU6fAACUAjnhs6t8UmVeaD45s34ERdg";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
